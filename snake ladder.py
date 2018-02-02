7e55bc5dd0935e448520f9676f3419b3ac90fdeb
17BTEC023 <35588008+17BTEC023@users.noreply.github.com>
2018-02-02 09:38:20 +0530

    Add files via upload

---
+++ snake ladder.py
@@ -0,0 +1,105 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: /home/ec2017b206/ladder.py ====================
+press r to roll a dicer
+Traceback (most recent call last):
+  File "/home/ec2017b206/ladder.py", line 7, in <module>
+    r=random.randit(1,6)
+AttributeError: module 'random' has no attribute 'randit'
+>>> 
+==================== RESTART: /home/ec2017b206/ladder.py ====================
+press r to roll a dicer
+Traceback (most recent call last):
+  File "/home/ec2017b206/ladder.py", line 7, in <module>
+    r=random.randit(1,6)
+AttributeError: module 'random' has no attribute 'randit'
+>>> 
+==================== RESTART: /home/ec2017b206/ladder.py ====================
+press r to roll a dicer
+your random number is 6
+you are in position False
+6
+continue playing
+press r to roll a dicer
+your random number is 3
+you are in position False
+3
+continue playing
+press r to roll a dicer
+your random number is 3
+you are in position False
+3
+continue playing
+press r to roll a dicer
+your random number is 5
+you are in position False
+5
+continue playing
+press r to roll a dicer
+your random number is 5
+you are in position False
+5
+continue playing
+press r to roll a dicer
+your random number is 1
+you are in position False
+1
+continue playing
+press r to roll a dicer
+your random number is 5
+you are in position False
+5
+continue playing
+press r to roll a dicer
+your random number is 6
+you are in position False
+6
+continue playing
+press r to roll a dicer
+your random number is 6
+you are in position False
+6
+continue playing
+press r to roll a dicer
+your random number is 4
+you are in position False
+4
+continue playing
+press r to roll a dicer
+your random number is 5
+you are in position False
+5
+continue playing
+press r to roll a dicer
+your random number is 1
+you are in position False
+1
+continue playing
+press r to roll a dicer
+your random number is 6
+you are in position False
+6
+continue playing
+press r to roll a dicer
+your random number is 3
+you are in position False
+3
+continue playing
+press r to roll a dicer
+your random number is 2
+you are in position False
+2
+continue playing
+press r to roll a dicer
+your random number is 1
+you are in position False
+1
+continue playing
+press r to roll a dicer
+your random number is 1
+you are in position False
+1
+continue playing
+press r to roll a dice
