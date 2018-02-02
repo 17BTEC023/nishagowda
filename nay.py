820bafb3adfc6f93ef7a5344603a0ce159c63765
17BTEC023 <35588008+17BTEC023@users.noreply.github.com>
2018-02-02 10:23:43 +0530

    Add files via upload

---
+++ nay.py
@@ -0,0 +1,52 @@
+import random
+count=0
+while( count<=100
+if roll=='r'
+
+    roll=input("enter random no is ",r)
+    print("you are in position",count)
+    count=count+r
+    print(r)
+    if count>100:
+        print("your no is greater than 100")
+        count=count-r
+        print("count value")
+
+    elif count==100:
+        print("you win")
+        break
+    elif count==8:
+         count=37
+         print("move up")
+    elif count==11:
+         count=2
+         print("snake bit")
+    elif count==13:
+         count=34
+         print("move up")
+    elif count==25:
+         count=4
+         print("snake bit")
+    elif count==38:
+         count=9
+         print("snake bit")
+    elif count==40:
+         count=68
+         print("move up")
+    elif count==52:
+        count=81
+        print("move up")
+    elif count==65:
+         count=46
+         print("snake bit")
+    elif count==76:
+         count=97
+         print("move up")
+    elif count==89:
+         count=70
+         print("snake bit")
+    elif count==93:
+         count=64
+         print("snake bit")
+    
+ 
