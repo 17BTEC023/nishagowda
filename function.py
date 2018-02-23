1db44e1302c7f85d6078c199d33f0ba304b4bac7
17BTEC023 <35588008+17BTEC023@users.noreply.github.com>
2018-02-23 10:20:00 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,24 @@
+print("using add function")
+def add (x,y):
+    return x+y
+print(add(2,5))
+
+print("using sub function")
+def sub (x,y):
+    return x-y
+print(sub(2,5))
+
+print ("using mul function")
+def mul (x,y):
+    return x*y
+print(mul(2,5))
+
+print ("using div function")
+def div (x,y):
+    return x%y
+print (div(2,5))
+
+print("using floor function")
+def flo (x,y):
+    return x\\y 
+print (flo(2,5))
